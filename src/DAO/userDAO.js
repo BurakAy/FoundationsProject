85d60dce116343a954633aa6b02452cefd27a09c
Burak <14030652+BurakAy@users.noreply.github.com>
2024-09-18 11:44:08 -0700

    Wrote logic for user login and account creation

---
+++ src/DAO/userDAO.js
@@ -0,0 +1,62 @@
+const {
+  DynamoDBClient,
+  QueryCommand,
+  ConditionalCheckFailedException
+} = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  PutCommand,
+  GetCommand
+} = require("@aws-sdk/lib-dynamodb");
+
+const client = new DynamoDBClient({ region: "us-west-1" });
+const docClient = DynamoDBDocumentClient.from(client);
+
+const TableName = "User";
+let userId = 1;
+
+async function userInfo(userName) {}
+
+async function userLogin(userName, pass) {
+  const command = new QueryCommand({
+    TableName,
+    KeyConditionExpression: "userName = :userName",
+    FilterExpression: "password = :password",
+    ExpressionAttributeValues: {
+      ":userName": { S: userName },
+      ":password": { S: pass }
+    }
+  });
+  try {
+    const response = await docClient.send(command);
+    return response.Items;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function createAccount(username, pass) {
+  const command = new PutCommand({
+    TableName,
+    Item: {
+      userName: username,
+      password: pass,
+      role: "employee",
+      userId: (userId += 1)
+    },
+    ConditionExpression: "attribute_not_exists(userName)"
+  });
+  try {
+    const response = await docClient.send(command);
+    console.log("RESPONSE: ", response.error);
+    return response;
+  } catch (err) {
+    if (err instanceof ConditionalCheckFailedException) {
+      return false;
+    } else {
+      console.log(err);
+    }
+  }
+}
+
+module.exports = { userLogin, createAccount };
