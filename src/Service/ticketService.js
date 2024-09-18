d625bf19f2d6978737dc5fd2a7873cc0a1294259
Burak <14030652+BurakAy@users.noreply.github.com>
2024-09-18 13:58:45 -0700

    Logic for handling submission and validation

--- src/Service/ticketService.js
+++ src/Service/ticketService.js
@@ -5,7 +5,25 @@ const {
   employeeTickets
 } = require("../DAO/TicketDAO");
 
-function submitTicket(ticket) {}
+async function submitTicket(ticket) {
+  const response = { status: null, message: "" };
+  if (ticket.amount && ticket.description && ticket.userName) {
+    const newTicket = await createTicket(ticket);
+    if (newTicket == 200) {
+      response.status = 200;
+      response.message = "Ticket successfully submitted";
+    } else {
+      response.status = 400;
+      response.message = "Failed to submit ticket";
+    }
+
+    return response;
+  }
+
+  response.status = 400;
+  response.message = "Tickets must have an amount, description, and username";
+  return response;
+}
 
 function processTicket(ticketId) {}
 
