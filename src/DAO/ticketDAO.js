a96ec006fd6e01ffb7d65199a6d6e3f5210857d3
Burak <14030652+BurakAy@users.noreply.github.com>
2024-09-18 13:57:44 -0700

    Logic for adding a ticket to the db

--- src/DAO/ticketDAO.js
+++ src/DAO/ticketDAO.js
@@ -5,13 +5,26 @@ const {
   UpdateCommand,
   DeleteCommand
 } = require("@aws-sdk/lib-dynamodb");
+const uuid = require("uuid");
 
 const client = new DynamoDBClient({ region: "us-west-1" });
 const docClient = DynamoDBDocumentClient.from(client);
 
 const TableName = "Ticket";
+let ticketId = uuid.v4();
 
-async function createTicket(ticket) {}
+async function createTicket(ticket) {
+  const command = new PutCommand({
+    TableName,
+    Item: { ...ticket, ticketId: ticketId, status: "pending" }
+  });
+  try {
+    const response = await docClient.send(command);
+    return response.$metadata.httpStatusCode;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 async function updateTicket(ticketId) {}
 
