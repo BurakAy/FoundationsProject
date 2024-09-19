b3ab0af32003f8a1449a566e01428b6b1306778d
Burak <14030652+BurakAy@users.noreply.github.com>
2024-09-19 09:57:47 -0700

    Moved logic to Controller for use of router

--- src/app.js
+++ src/app.js
@@ -1,54 +1,15 @@
 const express = require("express");
-const { login, register } = require("./Service/UserService");
-const { submitTicket, processTicket } = require("./Service/TicketService");
-
+const systemRouter = require("./Controller/SystemRouter");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post("/login", async (req, res) => {
-  const loggedIn = await login(req.body);
-  if (loggedIn) {
-    res.status(200);
-    res.send("Login successful");
-  } else {
-    res.status(401);
-    res.send("Login failed, check user credentials");
-  }
+app.use((req, res, next) => {
+  next();
 });
 
-app.post("/register", async (req, res) => {
-  const registered = await register(req.body);
-  if (registered) {
-    res.status(200);
-    res.send("Registration successful");
-  } else {
-    res.status(409);
-    res.send("Username already exists");
-  }
-});
-
-app.post("/submit-ticket", async (req, res) => {
-  const ticket = await submitTicket(req.body);
-  res.status(ticket.status);
-  res.send(ticket.message);
-});
-
-app.put("/process-ticket", async (req, res) => {
-  const ticket = await processTicket(req.body);
-  console.log(ticket);
-  res.status(ticket.status);
-  res.send({ message: ticket.message, updatedTicket: ticket.updatedTicket });
-});
-
-app.get("/employee-tickets/:userName", (req, res) => {
-  res.send(req.params);
-});
-
-app.get("/pending-tickets", (req, res) => {
-  res.send("pending");
-});
+app.use("/", systemRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
