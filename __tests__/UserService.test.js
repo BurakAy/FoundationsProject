bc3f0a5a4e2f7be62f9bbee7ab3a6e9b65200e11
Burak <14030652+BurakAy@users.noreply.github.com>
2024-09-20 12:55:57 -0700

    Created empty tests for login functionality

--- __tests__/UserService.test.js
+++ __tests__/UserService.test.js
@@ -1,5 +1,19 @@
-// const { login, register } = require("../src/Service/UserService");
+const { login, register } = require("../src/Service/UserService");
+const { userLogin, createAccount } = require("../src/DAO/UserDAO");
 
-// describe("User account functionality tests", () => {
+jest.mock("../src/DAO/UserService", () => ({
+  userLogin: jest.fn(),
+  createAccount: jest.fn()
+}));
 
-// })
+describe("User account login functionality tests", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("should return a message with login successful", () => {});
+
+  test("should fail to login due to wrong credentials", () => {});
+
+  test("should fail to login due to missing credentials", () => {});
+});
