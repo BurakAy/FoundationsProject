a478f292146122865ac4a0c563ff884dbb14c986
Burak <14030652+BurakAy@users.noreply.github.com>
2024-09-24 09:23:09 -0700

    Rearranged order of functions

--- src/Controller/SystemRouter.js
+++ src/Controller/SystemRouter.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// const bcrypt = require("bcrypt");
-
 const { login, register } = require("../Service/UserService");
 const {
   submitTicket,
@@ -11,35 +9,36 @@ const {
   previousTickets
 } = require("../Service/TicketService");
 
-router.post("/login", async (req, res) => {
-  const loggedIn = await login(req.body);
-  res.status(loggedIn.status);
-  res.send(loggedIn.message);
-});
-
 router.post("/register", async (req, res) => {
   const registered = await register(req.body);
   res.status(registered.status);
   res.send(registered.message);
 });
 
-router.post("/submit-tickets", async (req, res) => {
+router.post("/login", async (req, res) => {
+  const loggedIn = await login(req.body);
+  res.status(loggedIn.status);
+  res.send(loggedIn.message);
+});
+
+router.get("/tickets", async (req, res) => {
+  // const tickets = await previousTickets(req.params.userName);
+  const tickets = await previousTickets();
+  res.status(tickets.status);
+  if (tickets.submitted) {
+    res.send({ message: tickets.message, submissions: tickets.submitted });
+  } else {
+    res.send(tickets.message);
+  }
+});
+
+router.post("/tickets/submit", async (req, res) => {
   const ticket = await submitTicket(req.body);
   res.status(ticket.status);
   res.send(ticket.message);
 });
 
-router.put("/process-tickets", async (req, res) => {
-  const ticket = await processTicket(req.body);
-  res.status(ticket.status);
-  if (ticket.updatedTicket) {
-    res.send({ message: ticket.message, updatedTicket: ticket.updatedTicket });
-  } else {
-    res.send(ticket.message);
-  }
-});
-
-router.get("/pending-tickets", async (req, res) => {
+router.get("/tickets/pending", async (req, res) => {
   const tickets = await ticketsPending();
   res.status(tickets.status);
   if (tickets.pending) {
@@ -49,13 +48,13 @@ router.get("/pending-tickets", async (req, res) => {
   }
 });
 
-router.get("/employee-tickets/:userName", async (req, res) => {
-  const tickets = await previousTickets(req.params.userName);
-  res.status(tickets.status);
-  if (tickets.submitted) {
-    res.send({ message: tickets.message, submissions: tickets.submitted });
+router.put("/tickets/process", async (req, res) => {
+  const ticket = await processTicket(req.body);
+  res.status(ticket.status);
+  if (ticket.updatedTicket) {
+    res.send({ message: ticket.message, updatedTicket: ticket.updatedTicket });
   } else {
-    res.send(tickets.message);
+    res.send(ticket.message);
   }
 });
 
