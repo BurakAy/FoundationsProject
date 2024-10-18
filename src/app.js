82ef2726ea168cee709ff181f93f1c97e3b5ec0a
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-18 14:50:11 -0700

    Changed port

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("./util/logger");
 const systemRouter = require("./Controller/SystemRouter");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.json());
 
